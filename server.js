890cb313913f6336ba1b651cd25ec22ff3254a69
Aitor González Fernández <reimashi@gmail.com>
2015-07-03 22:24:20 +0200

    Minor fix

--- server.js
+++ server.js
@@ -12,42 +12,7 @@ var winston         = require('winston');
 
 var bruteforce      = require(path.join(__dirname, '/app/libs/bruteforce'));
 
-var self            = this;
 var config          = require('./app/config');
-
-self.setupVariables = function() {
-    //  Set the environment variables we need.
-    self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-    self.port      = process.env.OPENSHIFT_NODEJS_PORT || config.http.port;
-
-    if (typeof self.ipaddress === "undefined") {
-        //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-        //  allows us to run/test the app locally.
-        console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-        self.ipaddress = "127.0.0.1";
-    };
-};
-
-self.terminator = function(sig){
-    if (typeof sig === "string") {
-       console.log('%s: Received %s - terminating sample app ...',
-                   Date(Date.now()), sig);
-       process.exit(1);
-    }
-    console.log('%s: Node server stopped.', Date(Date.now()) );
-};
-
-self.setupTerminationHandlers = function(){
-   //  Process on exit and signals.
-   process.on('exit', function() { self.terminator(); });
-
-   // Removed 'SIGPIPE' from the list - bugz 852598.
-   ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-    'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-   ].forEach(function(element, index, array) {
-       process.on(element, function() { self.terminator(element); });
-   });
-};
 
 function configure() {
     app.set('x-powered-by', config.name);
@@ -118,15 +83,12 @@ process.on('SIGINT', function() {
 // Iniciando el servidor
 
 exports.start = function () {
-    self.setupVariables();
-    self.setupTerminationHandlers();
-
     winston.info("[server.js] Iniciando servidor web...");
     configure();
 
     // Iniciamos servidor HTTP
-    http.listen(self.port, function(){
-        winston.info("[server.js] Servicio HTTP iniciado (Puerto: " + (process.env.port || config.http.port) + ").");
+    http.listen((process.env.PORT || config.http.port), function(){
+        winston.info("[server.js] Servicio HTTP iniciado (Puerto: " + (process.env.PORT || config.http.port) + ").");
     });
 }
 
